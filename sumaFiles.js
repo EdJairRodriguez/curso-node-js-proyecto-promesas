cf778d1634d2bc8986d5209d96993ae2b99423a4
edson <edsonjairrodriguez@gmail.com>
2018-10-17 16:19:16 +0200

    s

--- sumaFiles.js
+++ sumaFiles.js
@@ -1,4 +1,19 @@
 const fs = require('fs')
-const numero1 = fs.readFileSync('./numero1', 'utf-8')
-const numero2 = fs.readFileSync('./numero2', 'utf-8')
-console.log(`El resultado de la suma es  ${parseInt(numero1)+parseInt(numero2)}`)
+
+fs.readFile('./numero1', (err, data) => {
+  if (err) console.log(err)
+  const numero1 = parseInt(data)
+  fs.readFile('./numero1', (err, data) => {
+    if (err) console.log(err)
+    const numero2 = parseInt(data)
+    console.log(`la suma es  ${numero1 + ${numero2}}`)
+  })
+})
+
+
+const customReadFile=(fileName)=>{
+  return fs.readFile('fileName',(err,data)=>{
+    if(err) console.log(err)
+    return const numero1=parseInt(data)
+  }
+}
